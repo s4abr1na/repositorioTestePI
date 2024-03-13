122c693e82586c56c9c09d0ec4906d0dd2c3a8c9
Sabrina <so38196@gmail.com>
2024-03-13 16:31:03 -0300

    cód de lógica

---
+++ Tarefa2_ExpressaoAritmetica.java
@@ -0,0 +1,18 @@
+
+public class Tarefa2_ExpressaoAritmetica {
+
+	public static void main(String[] args) {
+		// atribuição dos valores às respectivas variáveis 
+		int a = 2;
+		int b = 7;
+		float c = 3;
+		int d = 1;
+		
+		//variável que irá calcular a expressão e resultará em decimal
+		float resultado = (a + b) * c / (d - 3);
+		
+		//comando para apresentar o resultado da expressão aritmética
+		System.out.println("Resultado da expressão: " + resultado);
+	}
+ 
+}
